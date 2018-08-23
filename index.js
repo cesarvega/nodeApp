96f88b6ba0c96217159c63ce1f230ca7f4547ebb
Cesar Vega <cesarvega.col@gmail.com>
2018-08-23 09:35:28 -0400

    se agregan el modelo y rutas de scrumboard

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ appointmentRoutes = require('./server/routes/appointment.route');
 contactRoutes = require('./server/routes/contact.route');
 chatRoutes = require('./server/routes/chat.route');
 userRoutes = require('./server/routes/user.route');
+scrumboardRoutes = require('./server/routes/scrumboard.route');
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB).then(
   () => {console.log('Database is connected') },
@@ -53,6 +54,7 @@ app.use('/appointments', appointmentRoutes);
 app.use('/contact', contactRoutes);
 app.use('/chat', chatRoutes);
 app.use('/user', userRoutes);
+app.use('/scrumboards',scrumboardRoutes );
 // Parsers for POST data
 
 
--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 // DB.js
 
 module.exports = {
-    DB: 'mongodb://localhost:27017/bookappointments'    
-    // DB: 'mongodb://MRVR:megaman10@ds115472.mlab.com:15472/bookappointments'    
+    //DB: 'mongodb://localhost:27017/bookappointments'    
+    // DB: 'mongodb://MRVR:megaman10@ds115472.mlab.com:15472/bookappointments' 
+    DB: 'mongodb://localhost:27017/bookscrumboards'      
  };
