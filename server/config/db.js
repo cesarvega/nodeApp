052307d5442d9c2a1640453deaa5ae70d6768a3d
Cesar Vega <cesarvega.col@gmail.com>
2018-09-03 13:36:53 -0400

    repos ordenado chat y bd

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     //DB: 'mongodb://localhost:27017/bookappointments'    
-    DB: 'mongodb://MRVR:megaman10@ds141952.mlab.com:41952/chat-users' 
+    DB: 'mongodb://MRVR:megaman10@ds141932.mlab.com:41932/chat' 
     //DB: 'mongodb://localhost:27017/bookinfo'   
     //DB: 'mongodb://localhost:27017/bookadmins'
     //DB: 'mongodb://localhost:27017/bookscrumboards'
