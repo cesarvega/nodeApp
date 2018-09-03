b9233541a89000ceb36859e5c8fc403a4ece5c3a
Cesar Vega <cesarvega.col@gmail.com>
2018-09-03 14:34:40 -0400

    nuevo repos ordenado

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,8 @@
 // DB.js
 
 module.exports = {
-    //DB: 'mongodb://localhost:27017/bookappointments'    
+     
     DB: 'mongodb://MRVR:megaman10@ds141932.mlab.com:41932/chat' 
-    //DB: 'mongodb://localhost:27017/bookinfo'   
-    //DB: 'mongodb://localhost:27017/bookadmins'
-    //DB: 'mongodb://localhost:27017/bookscrumboards'
+   
     
  };
