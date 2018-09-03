a5c4ee4fddb9a12852f5e34e32cc6a4b5b44e26b
Cesar Vega <cesarvega.col@gmail.com>
2018-09-03 12:11:39 -0400

    repost ordenado chat

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,9 @@
 
 module.exports = {
     //DB: 'mongodb://localhost:27017/bookappointments'    
-    //DB: 'mongodb://MRVR:megaman10@ds135852.mlab.com:35852/bookinfo' 
+    DB: 'mongodb://MRVR:megaman10@ds141952.mlab.com:41952/chat-users' 
     //DB: 'mongodb://localhost:27017/bookinfo'   
-    DB: 'mongodb://localhost:27017/bookadmins'
+    //DB: 'mongodb://localhost:27017/bookadmins'
+    //DB: 'mongodb://localhost:27017/bookscrumboards'
     
  };
