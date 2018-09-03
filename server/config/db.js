9483e5bc6ee4ed2f5ff34fb5c6d2b0b6f8afa19f
Cesar Vega <cesarvega.col@gmail.com>
2018-09-03 12:37:16 -0400

    repos ordenado appointments

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,5 @@
 // DB.js
 
-module.exports = {
-    //DB: 'mongodb://localhost:27017/bookappointments'    
-    //DB: 'mongodb://MRVR:megaman10@ds135852.mlab.com:35852/bookinfo' 
-    //DB: 'mongodb://localhost:27017/bookinfo'   
-    DB: 'mongodb://localhost:27017/bookadmins'
-    
+module.exports = {      
+    DB: 'mongodb://MRVR:megaman10@ds141952.mlab.com:41952/appointments'    
  };
